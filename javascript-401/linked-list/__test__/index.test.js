732ba977c907108ee312a82db643fe79907d6ee5
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-11-21 22:59:08 +0200

    edittt

---
+++ javascript-401/linked-list/__test__/index.test.js
@@ -0,0 +1,287 @@
+'use srtict';
+
+const LinkedList = require('../lib/LinkedList');
+
+describe('Linked list', () => {
+
+  it('creation of linkedList works', () => {
+    let list = new LinkedList();
+    expect(list.head).toBeNull();
+  });
+
+  it('Can successfully instantiate an empty linked list', () => {
+    let list = new LinkedList();
+
+    list.insert('One');
+    expect(list.head.value).toEqual('One');
+  });
+
+  it('Can properly insert into the linked list', () => {
+    let list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toBe('{c} -> {b} -> {a} -> Null');
+  });
+
+  it('The head property will properly point to the first node in the linked list', () => {
+    let list = new LinkedList();
+    list.insert('One');
+    list.insert('Two');
+
+    expect(list.head.value).toEqual('Two');
+  });
+
+  it(' Will return true when finding a value within the linked list that exists', () => {
+    let list = new LinkedList();
+
+    list.insert('a');
+    list.insert('b');
+    let isExist = list.includes('b');
+    expect(isExist).toBe(true);
+  });
+
+  it(' Will return false when searching for a value in the linked list that does not exist', () => {
+    let list = new LinkedList();
+
+    list.insert('a');
+    list.insert('b');
+    let isExist = list.includes('c');
+    expect(isExist).toBe(false);
+  });
+
+  it('Can properly return a collection of all the values that exist in the linked list', () => {
+    let list = new LinkedList();
+    list.insert('a');
+    list.insert('b');
+    list.insert('c');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toBe('{c} -> {b} -> {a} -> Null');
+  });
+
+  //////////////////////////////////////////// Challenge 06 testing //////////////////////////////////////
+
+  test('Can successfully add a node to the end of the linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+
+    expect(list.tail.value).toEqual('2');
+  });
+
+  test('Can successfully add multiple nodes to the end of a linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.append('3');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toEqual('{1} -> {2} -> {3} -> Null');
+  });
+
+  test('Can successfully insert a node before a node located i the middle of a linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.insertBefore('2', '3');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toEqual('{1} -> {3} -> {2} -> Null');
+  });
+
+  test('Can successfully insert a node before the first node of a linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.insertBefore('1', '3');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toEqual('{3} -> {1} -> {2} -> Null');
+  });
+
+  test('Can successfully insert after a node in the middle of the linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.insertAfter('1', '3');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toEqual('{1} -> {3} -> {2} -> Null');
+  });
+
+  test('Can successfully insert a node after the last node of the linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.insertAfter('2', '3');
+    list.toString();
+
+    expect(list.returnedLinkedlList).toEqual('{1} -> {2} -> {3} -> Null');
+  });
+
+  //////////////////////////////////////////// Challenge 07 testing //////////////////////////////////////
+
+  it('Where k is greater than the length of the linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.append('3');
+    list.append('4');
+
+    // the lenght of linked list is 3 (4 ithems)
+    let kthValue = list.kthFromEnd(5);
+    expect(kthValue).toBeNull();
+  });
+
+  it('Where k and the length of the list are the same', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.append('3');
+    list.append('4');
+
+    // the lenght of linked list is 3 (4 ithems)
+    let kthValue = list.kthFromEnd(3);
+    expect(kthValue).toBe('1');
+  });
+
+  it('Where k is not a positive integer', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.append('3');
+    list.append('4');
+
+    let kthValue = list.kthFromEnd(-1);
+    expect(kthValue).toBeNull();
+  });
+
+  it('Where the linked list is of a size 1', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+
+    let kthValue = list.kthFromEnd(0);
+    expect(kthValue).toBe('1');
+
+    kthValue = list.kthFromEnd(1);
+    expect(kthValue).toBeNull();
+  });
+
+  it('“Happy Path” where k is not at the end, but somewhere in the middle of the linked list', () => {
+    let list = new LinkedList();
+
+    list.append('1');
+    list.append('2');
+    list.append('3');
+    list.append('4');
+    list.append('5');
+
+    let kthValue = list.kthFromEnd(2);
+    expect(kthValue).toBe('3');
+  });
+
+  //////////////////////////////////////////// Challenge 08 testing //////////////////////////////////////
+
+  test('ziplists function', () => {
+    let listA = {
+      head: {
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: 3,
+            next: null,
+          },
+        },
+      },
+    };
+
+    let listb = {
+      head: {
+        value: 4,
+        next: {
+          value: 5,
+          next: {
+            value: 6,
+            next: null,
+          },
+        },
+      },
+    };
+
+    let list = new LinkedList();
+
+    list.zipLists(listA, listb);
+
+    expect(list.toString()).toEqual(
+      '{1} -> {4} -> {2} -> {5} -> {3} -> {6} -> Null'
+    );
+
+    listA = {
+      head: {
+        value: 1,
+        next: {
+          value: 2,
+          next: null,
+        },
+      },
+    };
+
+    listb = {
+      head: {
+        value: 4,
+        next: {
+          value: 5,
+          next: {
+            value: 6,
+            next: null,
+          },
+        },
+      },
+    };
+
+    list.zipLists(listA, listb);
+
+    expect(list.toString()).toEqual('{1} -> {4} -> {2} -> {5} -> {6} -> Null');
+
+    listA = {
+      head: {
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: 3,
+            next: null
+          },
+        },
+      },
+    };
+
+    listb = {
+      head: {
+        value: 4,
+        next: {
+          value: 5,
+          next: null,
+        },
+      },
+    };
+
+    list.zipLists(listA, listb);
+
+    expect(list.toString()).toEqual('{1} -> {4} -> {2} -> {5} -> {3} -> Null');
+  });
+});
---
+++ javascript-401/linked-list/lib/LinkedList.js
@@ -0,0 +1,219 @@
+'use strict';
+
+const Node = require('../lib/Node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.valueueExists = false;
+    this.returnedLinkedlList = ``;
+  }
+
+
+  append(value) {
+    try {
+      const newNode = new Node(value);
+
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+        return this;
+      }
+
+      let currentNode = this.tail;
+
+      currentNode.next = newNode;
+
+      this.tail = currentNode.next;
+
+      return this;
+    } catch (e) {
+      console.error(`error in appending a new value to the linked list`);
+    }
+  }
+  insertBefore(value, newValue) {
+    try {
+      const newNode = new Node(newValue);
+
+      if (!this.head) {
+        this.head = newNode;
+        return this;
+      }
+
+      let currentNode = this.head;
+      let previousNode;
+
+      if (currentNode.value === value) {
+        this.head = newNode;
+        this.head.next = currentNode;
+      } else {
+        while (currentNode.next) {
+          if (currentNode.value === value) {
+            break;
+          }
+          previousNode = currentNode;
+          currentNode = currentNode.next;
+        }
+        previousNode.next = newNode;
+        let addedNode = previousNode.next;
+
+        addedNode.next = currentNode;
+      }
+
+      return this;
+    } catch (e) {
+      console.error(
+        `error in inserting ${value} before ${newValue} to the linked list`
+      );
+    }
+  }
+  insertAfter(value, newValue) {
+    try {
+      const newNode = new Node(newValue);
+
+      if (!this.head) {
+        this.head = newNode;
+        return this;
+      }
+
+      let currentNode = this.head;
+
+      while (currentNode.next) {
+        if (currentNode.value === value) {
+          break;
+        }
+        currentNode = currentNode.next;
+      }
+      let afterNewNode = currentNode.next;
+      currentNode.next = newNode;
+      let addedNode = currentNode.next;
+      addedNode.next = afterNewNode;
+
+      return this;
+    } catch (e) {
+      console.error(
+        `error in inserting ${value} after ${newValue} to the linked list`
+      );
+    }
+  }
+
+  insert(value) {
+    try {
+      const newNode = new Node(value);
+
+      if (!this.head) {
+        this.head = newNode;
+        return this;
+      }
+
+      let oldHead = this.head;
+
+      this.head = newNode;
+
+      this.head.next = oldHead;
+
+      return this;
+    } catch (e) {
+      console.error(`error in inserting a new value to the linked list`);
+    }
+  }
+  includes(value) {
+    try {
+      let currentNode = this.head;
+
+      while (currentNode.next) {
+        if (currentNode.value === value) {
+          this.valueueExists = true;
+          break;
+        }
+        currentNode = currentNode.next;
+      }
+
+      // to check the last node
+      if (currentNode.value === value) {
+        this.valueueExists = true;
+      }
+
+      return this.valueueExists;
+    } catch (e) {
+      console.error('error in searching at the linked list');
+    }
+  }
+  toString() {
+    let currentNode = this.head;
+
+    this.returnedLinkedlList = `{${this.head.value}}`;
+
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+      this.returnedLinkedlList =
+        this.returnedLinkedlList + ` -> {${currentNode.value}}`;
+    }
+
+    this.returnedLinkedlList = this.returnedLinkedlList + ` -> Null`;
+
+    // console.log(this.returnedLinkedlList);
+
+    return this.returnedLinkedlList;
+  }
+
+  kthFromEnd(k) {
+    try {
+      if (!this.head) {
+        return null;
+      }
+
+      let currentNode = this.head;
+      let nodevalueuesArr = [];
+      while (currentNode.next) {
+        nodevalueuesArr.push(currentNode.value);
+        currentNode = currentNode.next;
+      }
+      nodevalueuesArr.push(currentNode.value);
+
+      if (k >= nodevalueuesArr.length || k < 0) {
+        return null;
+      }
+
+      for (let i = 0; i < nodevalueuesArr.length; i++) {
+        if (nodevalueuesArr.length - 1 - k === i) {
+          // console.log( nodevalueuesArr[i]);
+          return nodevalueuesArr[i];
+        }
+      }
+    } catch (e) {
+      console.error(`error in returning kth from the end for the linked list`);
+    }
+  }
+
+  zipLists(a, b) {
+    let aNode = a.head;
+    let bNode = b.head;
+
+    let currentNode = new Node(aNode.value);
+    this.head = currentNode;
+
+
+    while ((currentNode && bNode) || (currentNode && aNode)) {
+      if (bNode) {
+        currentNode.next = new Node(bNode.value);
+        currentNode = currentNode.next;
+        bNode = bNode.next;
+      }
+
+      if (aNode) {
+        aNode = aNode.next;
+        if (aNode) {
+          currentNode.next = new Node(aNode.value);
+          currentNode = currentNode.next;
+        }
+      }
+    }
+    return this.head;
+
+
+  }
+}
+
+module.exports = LinkedList;
