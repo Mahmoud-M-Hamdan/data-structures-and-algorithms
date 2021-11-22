d7d86d905aea629977b3ac4d19abf4b870ed043d
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-11-23 00:23:40 +0200

    ch17

--- javascript-401/tree/BinaryTree.js
+++ javascript-401/tree/BinaryTree.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const Node = require("./Node");
+const Queue = require("../stack-and-queue/Queue");
 
 class BinaryTree {
   constructor(root = null) {
@@ -119,6 +120,62 @@ class BinaryTree {
 
     return maximumValue;
   }
+
+
+  pre() {
+    if (!this.root) {
+      return "Binary tree is empty";
+    }
+
+    let preOrderArr = [];
+
+    const _walk = (node) => {
+      // read
+      preOrderArr.push(node.value);
+      // check left
+      if (node.leftChild) {
+        _walk(node.leftChild);
+      }
+      // check right
+      if (node.rightChild) {
+        _walk(node.rightChild);
+      }
+    };
+
+    _walk(this.root);
+
+    return preOrderArr;
+  }
+
+
+  breadthFirst() {
+    let breadthQueue = new Queue();
+  
+    if (!this.root) {
+      return "The tree is empty";
+    }
+  
+    breadthQueue.enqueue(this.root);
+  
+    let breadthFirstArr = [];
+  
+    while (breadthQueue.peek()) {
+      let front = breadthQueue.dequeue();
+  
+      breadthFirstArr.push(front.value);
+  
+      if (front.leftChild) {
+        breadthQueue.enqueue(front.leftChild);
+      }
+  
+      if (front.rightChild) {
+        breadthQueue.enqueue(front.rightChild);
+      }
+    }
+  
+    return breadthFirstArr;
+  }
+
 }
 
 module.exports = BinaryTree;
